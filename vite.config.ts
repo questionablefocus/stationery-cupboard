0d6449e37ab5ffd7f0579b35e1448bdd221184ec
Marcus Mo <marcus.mo@questionablefocus.com>
2025-03-25 22:56:28 +0000

    Start using tailwind v4 vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   esbuild: {
     jsx: "automatic", // Ensure JSX is properly transformed
     jsxImportSource: "react", // Ensure React handles JSX
